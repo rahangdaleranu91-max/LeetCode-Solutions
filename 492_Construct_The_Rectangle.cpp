3829a58a18eedc28dc09e68bcd2d996278886b92
rahangdaleranu91-max <rahangdaleranu91@gmail.com>
2026-07-28 13:11:10 +0530

    Create 492_Construct_The_Rectangle.cpp

---
+++ 492_Construct_The_Rectangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+
+        int w=sqrt(area);
+        while(area%w!=0)
+        {
+            w--;
+        }
+
+        return {area/w,w};
+    }
+};        
