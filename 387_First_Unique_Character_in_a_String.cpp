dcf11815c00f0118e6002cb529ddda3b142b268c
rahangdaleranu91-max <rahangdaleranu91@gmail.com>
2026-08-09 09:08:10 +0530

    Create 387_First_Unique_Character_in_a_String.cpp

---
+++ 387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        int freq[26] = {0};
+
+        // Step 1: count frequency
+        for(char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        // Step 2: find first character with frequency 1
+        for(int i = 0; i < s.length(); i++) {
+            if(freq[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
